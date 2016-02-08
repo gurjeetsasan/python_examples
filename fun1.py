b562003ba79bfef02d8480849af3a8b43aec9ec1
Vipan Kumar <websolutions.artworld@gmail.com>
2016-02-08 12:23:09 +0530

    Program to functions

---
+++ fun1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def main():
+    print("main code")
+
+def numberRange( num ):
+	print "Number: ", num
+
+for i in range(0,10):
+	numberRange(i)
+
+
+if __name__ == "__main__":main()
